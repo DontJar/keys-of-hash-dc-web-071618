76be1d8e40b5c1261b6b1b5738c97f0c40997df0
Jon Schulman <schulman99@gmail.com>
2018-07-09 17:59:24 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,10 +4,9 @@ class Hash
   def keys_of(arguments)
   what_u_want = []
     # arr << self.key(arguments)
-    self.each do |animal, location|
-      if location == arguments
-        what_u_want << animal
-      end
+  self.each do |animal, location|
+    if location == arguments
+      what_u_want << animal
     end
   end
   what_u_want
