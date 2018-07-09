a277d2c9a9438ac71b90290c6823f4eb98a5ef6f
Jon Schulman <schulman99@gmail.com>
2018-07-09 18:15:55 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -11,7 +11,7 @@ binding.pry
   end
   if others.size >0
     self.each do |keys_in_hash, vals_in_hash|
-      if vals_in_hash == arguments
+      if vals_in_hash == others.join
         what_u_want << keys_in_hash
       end
     end
