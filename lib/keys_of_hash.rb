9eb7d32c5c9debaae27f292cbff43f93ea3a77e4
Jon Schulman <schulman99@gmail.com>
2018-07-09 18:13:57 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -9,6 +9,12 @@ class Hash
       what_u_want << keys_in_hash
     end
   end
+  if *others
+    self.each do |keys_in_hash, vals_in_hash|
+      if vals_in_hash == arguments
+        what_u_want << keys_in_hash
+      end
+    end
   what_u_want
 end
 end
