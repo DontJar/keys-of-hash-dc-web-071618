984ae15fcf2144ab992bcb5cb9128853679d44f1
Jon Schulman <schulman99@gmail.com>
2018-07-09 17:30:38 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,9 +1,17 @@
 require "pry"
 
+# class Hash
+#   def keys_of(arguments)
+#   arr = []
+#     arr << self.key(arguments)
+#     binding.pry
+#   end
+# end
+
 class Hash
   def keys_of(arguments)
   arr = []
-    arr << self.key(arguments)
+    self.key(arguments)
     binding.pry
   end
 end
